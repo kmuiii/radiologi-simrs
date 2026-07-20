7e90eeb446a71b8e10d450d834548d9f5c4bdefe
kmuiii <pratamaareza111@gmail.com>
2026-07-20 13:17:22 +0700

    refactor: update tabel master poli

--- database/migrations/2026_07_20_025235_create_master_poli_table.php
+++ database/migrations/2026_07_20_025235_create_master_poli_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('master_poli', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('code')->nullable();
             $table->timestamps();
         });
     }
