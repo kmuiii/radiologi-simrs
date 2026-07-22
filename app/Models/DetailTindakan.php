e730ed2f8a2e54ade5f533e28d4a86a33bd5490d
kmuiii <pratamaareza111@gmail.com>
2026-07-22 09:27:32 +0700

    fix: typo titik koma dll di DetailTindakan

--- app/Models/DetailTindakan.php
+++ app/Models/DetailTindakan.php
@@ -5,20 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 
-#[Fillable(['tindakan_id','examination_type_id', 'radiologist_id', 'price', 'status'])];
+#[Fillable(['tindakan_id','examination_type_id', 'radiologist_id', 'price', 'status'])]
 class DetailTindakan extends Model
 {
+
     protected $table = 'detail_tindakan';
 
-    protected $casts = ['price' => 'decimal:2']
+    protected $casts = ['price' => 'decimal:2'];
 
     protected static function booted()
     {
-        static::creating(function ($item)){
+        static::creating(function ($item) {
             if (empty($item->price)) {
                 $item->price = $item->examinationType->price;
             }
-        }
+        });
     }
 
     public function tindakan()
